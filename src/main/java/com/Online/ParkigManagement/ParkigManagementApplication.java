ebd9be143182b5330245b9862861fc79c93075e9
unknown <harshilkothiya2005@gmail.com>
2025-10-29 11:32:48 -0400

    commit message

---
+++ src/main/java/com/Online/ParkigManagement/ParkigManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Online.ParkigManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkigManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkigManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Online/ParkigManagement/ParkigManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online.ParkigManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkigManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
